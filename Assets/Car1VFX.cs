62666a85fec6d8575a55fe90a5e4b316ac99ee01
gngriffiths <g@untether.studio>
2022-10-10 12:56:20 +0100

    Updated sky

--- Assets/Car1VFX.cs
+++ Assets/Car1VFX.cs
@@ -3,36 +3,58 @@ using UnityEngine.AI;
 
 public class Car1VFX : MonoBehaviour
 {
-    [SerializeField] ParticleSystem[] jetParticles = null;
+    [SerializeField] bool attacking;
 
-    NavMeshAgent navMeshAgent
-    {
-        get
-        {
-            if (navMeshAgentCached == null)
-                navMeshAgentCached = GetComponentInParent<NavMeshAgent>();
-            return navMeshAgentCached;
-        }
-    }
+    [SerializeField] ParticleSystem[] gunParticles = null;
 
-    NavMeshAgent navMeshAgentCached;
+    //NavMeshAgent navMeshAgent
+    //{
+    //    get
+    //    {
+    //        if (navMeshAgentCached == null)
+    //            navMeshAgentCached = GetComponentInParent<NavMeshAgent>();
+    //        return navMeshAgentCached;
+    //    }
+    //}
 
+    //NavMeshAgent navMeshAgentCached;
 
     private void Update()
     {
-        if (navMeshAgent.velocity.sqrMagnitude > 1)
-            VehicleMoving(true);
+        if (attacking)
+            Attacking(true);
         else
-            VehicleMoving(false);
+            Attacking(false);
+
+        attacking = false;
     }
 
-    void VehicleMoving(bool moving)
+    void Attacking(bool moving)
     {
-        if (moving && jetParticles[0].isStopped)
-            foreach (var particle in jetParticles)
+        if (moving && gunParticles[0].isStopped)
+            foreach (var particle in gunParticles)
                 particle.Play();
-        else if (!moving && jetParticles[0].isPlaying)
-            foreach (var particle in jetParticles)
+        else if (!moving && gunParticles[0].isPlaying)
+            foreach (var particle in gunParticles)
                 particle.Stop();
     }
+
+
+    //private void Update()
+    //{
+    //    if (navMeshAgent.velocity.sqrMagnitude > 1)
+    //        VehicleMoving(true);
+    //    else
+    //        VehicleMoving(false);
+    //}
+
+    //void VehicleMoving(bool moving)
+    //{
+    //    if (moving && jetParticles[0].isStopped)
+    //        foreach (var particle in jetParticles)
+    //            particle.Play();
+    //    else if (!moving && jetParticles[0].isPlaying)
+    //        foreach (var particle in jetParticles)
+    //            particle.Stop();
+    //}
 }
