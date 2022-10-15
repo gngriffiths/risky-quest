b9b7932856945ad3743d6bcb4bd4374f24b2f376
Johney16 <john.esl100@gmail.com>
2022-10-15 14:53:03 -0600

    audio-playoneshot script update

---
+++ Assets/Scripts/Audio/PlayAudio.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+  public class PlayAudio : MonoBehaviour
+    {
+        public AudioSource audioSource;
+        public AudioClip clip;
+        public float volume = 0.5f;
+        void Start()
+        {
+            audioSource.PlayOneShot(clip, volume);
+        }
+    }
